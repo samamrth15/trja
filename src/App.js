3e313e696b0ff3b6f30333db188753cd72dc16a3
sam <sam@gmail.com>
2023-12-09 14:35:40 +0530

    sa

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const Greeting = () => {
+  return <h1>Hello, Welcome to My React App!</h1>
+}
+
+function App() {
+  return (
+    <div>
+        <Greeting />
+    </div>
+  )
+}
+
+export default App
